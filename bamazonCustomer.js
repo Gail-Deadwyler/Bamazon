88ece9a7549ee7ceaae5e6f04ab93bfd2b8b981f
rivendell <webwyler@gmail.com>
2019-09-02 18:35:27 -0500

    added afterConnection() that shows item_id, product_name and price when app is first started

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,5 +15,21 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log(`Connected as ${connection.threadId}`);
+    afterConnection();
 });
+
+//Show all items in the products Table
+function afterConnection() {
+    var query = connection.query("SELECT * FROM products", function(err,res) {
+        if (err) throw err;
+        // console.log(res);
+        for (var i = 0; i < res.length; i++) {
+            console.log("Product ID: " + res[i].item_id + " ||" + " Product Name: " + res[i].product_name + " ||" + " Price: $" + parseInt(res[i].price));
+        }
+        console.log(`Query that is run: ${query.sql}`);
+        console.log("type of price is " + typeof(res[0].price));
+
+    });
+
+}
 
